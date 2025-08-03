7025343f51849ea64cc95d5cf901332c2a90df41
jknott1 <jknott@luc.edu>
2025-08-03 21:20:50 +0000

    finished merge and reflect

--- Merge.java
+++ Merge.java
@@ -46,6 +46,7 @@ public class Merge {
      * @param array int[] array to sort
      * @returns sorted int[]
      */
+
     static int[] mergeSort(int[] array) {
         // Initialize the output
         int[] result;
@@ -79,10 +80,117 @@ public class Merge {
         return result;
     } // method mergeSort
 
+    /**
+     * finds the maximum value with in a section of a given array.
+     *
+     * @param array with indices left and right indicating where in the array to look for a maximum value.
+     * @return the maximum value in the wanted bounds of the array.
+     */
+    public static int findMax(int[] array, int left, int right) {
+        // initializing return variable
+        int max;
+        // if the boundary is only one element long - return that element
+        if (left == right) {
+            max = array[left];
+        // if not:
+        } else {
+            // create a new array with the length of the bounds
+            int[] tempArr = new int[right - left + 1];
+            // assign it the values from the original array
+            for (int i = left; i <= right; i++) {
+                tempArr[i] = array[left + i];
+            }
+            // sort the list (recursive step)
+            tempArr = mergeSort(tempArr);
+            // take the max to be the last value, as the last value will be the largest in the list.
+            max = tempArr[tempArr.length - 1];
+        }
+        // return the max value
+        return max;
+    } // method findMax
+
+    /**
+     * reverses the given section of the array; the last element becomes the first, second to last becomes
+     * second, etc.
+     * @param array with left and right indices indicating boundaries.
+     */
+    public static void reverseArray(int[] array, int left, int right) {
+        // initialize a container variable
+        int temp;
+        // failsafe - if either boundary goes outside of the array, it will default to the entire array
+        if (right < 0 || left < 0 || right > array.length || left > array.length) {
+            left = 0;
+            right = array.length - 1;
+        }
+        // recursive loop
+        if (left <= right) {
+            // container variable records one of the switched element's value
+            temp = array[right];
+            // assigns the other value to the opposite position
+            array[right] = array[left];
+            // does the same with the first value
+            array[left] = temp;
+            // recursive step - calls the function again for the values in between the boundary,
+            // until the array is completely reversed
+            reverseArray(array, left + 1, right - 1);
+        }
+
+    } // method reverseArray
+
+    /**
+     * counts how many times a target value occurs within a given section in an array.
+     * @param array with a starting index and target value
+     * @return count of target value occurances
+     */
+    public static int countOccurances(int[] array, int target, int index) {
+        // initialize a counter variable
+        int count = 0;
+        // loop starts at index; count is increased by one if target is found
+        if  (array[index] == target) {
+            count += 1;
+        }
+        // moves on to the next index
+        index++;
+        // recursive step - so long as the index is no larger than the length of the array,
+        // calls itself again and adds another to count if it's found again.
+        if (index < array.length) {
+            count += countOccurances(array, target, index);
+        }
+        // after loop finishes, count is returned
+        return count;
+    } // method countOccurances
+
+    /**
+     * finds the targeted value within a section of an array, and returns it's index.
+     * @param array with left and right indices indicating the target section, and a target value
+     * @return the index of the target value
+     */
+    public static int binarySearch(int[] array, int target, int left, int right) {
+        // initializing return value with default index
+        int index = -1;
+        // uses boolean to track if target was found
+        boolean found = (array[left] == target);
+        // if found, assigns index to that position and ends loop
+        if  (found) {
+            index = left;
+        // recursive step - if not, the calls itself again with the next element's position
+        // so long as it does not exceed the bounds.
+        } else if (left < right && !found) {
+            index = binarySearch(array, target, left+1, right);
+        }
+        // returns the index
+        return index;
+    } // method binarySearch
+
+
     /** Demo code */
     public static void main(String[] args) {
         int[] test = { 4, 3, 2, 6 };
         int[] sortedTest = mergeSort(test);
+        reverseArray(sortedTest, 0, 3);
+        System.out.print(binarySearch(sortedTest, 3, 0, sortedTest.length-1) + "\n");
+        System.out.print(countOccurances(sortedTest, 6, 0) + "\n");
+        System.out.print(findMax(test, 0, sortedTest.length - 1));
         System.out.printf("\n%s\n\n",Arrays.toString(sortedTest));
     }
-}
+} // class Merge
---
+++ reflect.md
@@ -0,0 +1,7 @@
+# Reflection on Week 08 #
+
+This week, I think I did pretty well - the best I've done in a while. This is not necessarily because I thought my code was amazing, but moreso having to do with time management. As I am moving to a new home and with a couple major family events happening - I am pretty proud of myself for being able to keep up as normally I would have fallen behind. 
+
+In terms of my code, I think that I did pretty well. My code compiles, passes all the tests, and it structured similarly to the code in the solution (barring some stylistic choices). However, there is one difference that I wanted to discuss. In my code, I explicitly calculated the expected position of the node in question. However, the solution has a helper method `hashPosition` that calculates it outside of the method. Although this is a stylistic choice, this definitely would have helped the readability of the code. 
+
+One other thing - I added a method (or two) that wasn't used in the code, but was kept there as I thought it may have been useful in other contexts outside of the scope of this assignemnt. However, I recognize that it is also a somewhat unneeded piece of code, especially under what was asked of me. So this week, I finished just exactly what was asked of me.
